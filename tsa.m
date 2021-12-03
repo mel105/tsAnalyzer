3296fbf698a69264a7e4767059a3ccb7dc4676c2
MEL <m.elias@era.aero>
2021-12-03 15:46:13 +0100

    update main function

--- tsa.m
+++ tsa.m
@@ -2,16 +2,7 @@ function [res] = tsa(timeVec, valuesVec, cfg)
 
   % funkcia, ktora ma na vstupe pozdavovane vlastnostia  vrati pozadovanu structuru.
 
-  % odhad trendu
-  switch cfg.trendEstimation
-    case "Linear"
 
-      [rc, trendStruct] = linearRegression(timeVec, valuesVec);
-      plotLinearRegression(timeVec, valuesVec, trendStruct)
-    case "Harmonic"
-    otherwise
-      error("Required method is not implemented!");
-  end
 
   res = struct(...
     "trend", nan, ...
